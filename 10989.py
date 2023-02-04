2a49ded6a2f228ffa0a16799da2f96cb8cbd337a
Jae Ung Jung <roosa0210@gmail.com>
2023-02-04 13:15:59 +0900

    10989

---
+++ 10989.py
@@ -0,0 +1,13 @@
+import sys
+
+N = int(sys.stdin.readline())
+count =[0] * 10001
+
+for i in range(N):
+    count[int(sys.stdin.readline())] += 1
+
+for i in range(10001):
+    if(count[i]):
+        for j in range(count[i]):
+            print(i)
+
